8e70d5ba2d86acf917b14941d139c0ea2c83ebad
LONEL <97405216+LonelyPlugins@users.noreply.github.com>
2022-01-09 22:39:23 +0300

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Promocode.Config
-// Assembly: Promocode, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 084AF92B-0413-4443-9B63-8F15A32B5F70
-// Assembly location: C:\Users\Александр\Desktop\Promocode.dll
-
 using Rocket.API;
 using System.Collections.Generic;
 
